0b6d38218097d8561fa9e93ca0bab9e3beabddb3
Richard Ashman <richard.ashman@nhschoices.net>
2023-12-04 10:48:54 +0000

    Completed Part 1 of Day 4

---
+++ AdventOfCode.Tests/2023/Day 4/ScratchcardsTests.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode._2023.Day_4.Scratchcards;
+
+namespace AdventOfCode.Tests._2023.Day_4
+{
+    [TestClass]
+    public sealed class ScratchcardsTests
+    {
+        private static readonly string[] _rawScratchcards =
+            [
+                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+                "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+                "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+                "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+                "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+                "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            ];
+
+        [TestMethod]
+        public async Task Scratchcard_CorrectlyCalculatesThePointsEarnedByEachCard()
+        {
+            var rawScratchcards = _rawScratchcards.ToAsyncEnumerable();
+
+            var scratchcards = await rawScratchcards.SelectAwait(Scratchcard.ParseAsync).ToArrayAsync();
+
+            Assert.AreEqual(8, scratchcards[0].Points);
+            Assert.AreEqual(2, scratchcards[1].Points);
+            Assert.AreEqual(2, scratchcards[2].Points);
+            Assert.AreEqual(1, scratchcards[3].Points);
+            Assert.AreEqual(0, scratchcards[4].Points);
+            Assert.AreEqual(0, scratchcards[5].Points);
+        }
+
+        [TestMethod]
+        public async Task Scratchcard_CorrectlyCalculatesThePointsEarnedByEachCardInFile()
+        {
+            using var cts = new CancellationTokenSource(delay: TimeSpan.FromSeconds(5));
+
+            var rawScratchcards = File.ReadLinesAsync("2023/Day 4/Scratchcard-File.txt", cts.Token);
+
+            var scratchcards = rawScratchcards.SelectAwait(Scratchcard.ParseAsync);
+
+            var sumOfPoints = await scratchcards.SumAsync(c => c.Points);
+
+            Assert.AreEqual(21558, sumOfPoints);
+        }
+    }
+}
---
+++ AdventOfCode/2023/Day 4/Scratchcards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2023.Day_4.Scratchcards
+{
+    public sealed record Scratchcard(int CardIdx, int[] WinningNumbers, int[] CardNumbers, int Points)
+    {
+        public static ValueTask<Scratchcard> ParseAsync(string s)
+        {
+            var idxOfColon = s.IndexOf(':');
+            var idxOfPipe = s.IndexOf('|');
+
+            var cardIdx = int.Parse(s[5..idxOfColon++]);
+
+            var wns = s[idxOfColon..idxOfPipe];
+            var cns = s[(idxOfPipe + 1)..];
+
+            var winningNumbers = wns.Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(int.Parse).ToArray();
+            var cardNumbers = cns.Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(int.Parse).ToArray();
+
+            var intersect = cardNumbers.Intersect(winningNumbers).ToArray();
+
+            var points = intersect.Length switch {
+                0 => 0,
+                1 => 1,
+                _ => intersect.Aggregate(1, (a, _) => a + a) / 2
+            };
+
+            return ValueTask.FromResult(new Scratchcard(cardIdx, winningNumbers, cardNumbers, points));
+        }
+    }
+}
