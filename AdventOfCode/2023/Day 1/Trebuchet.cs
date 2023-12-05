1f537f758877c8148cf239de0d9a5fe7b7629532
Richard Ashman <richard.ashman@nhschoices.net>
2023-12-05 11:35:25 +0000

    Improvement to Day 1 solution to use a look ahead regex instead of a L2R and R2L pair

--- AdventOfCode/2023/Day 1/Trebuchet.cs
+++ AdventOfCode/2023/Day 1/Trebuchet.cs
@@ -19,30 +19,24 @@ namespace AdventOfCode._2023.Day_1
             {
                 var tokens = tokenizer.Tokenize(valueToBeCalibrated).ToArray();
 
-                if (2 > tokens.Length)
+                if (2 == tokens.Length)
                 {
-                    yield return default;
-                    continue;
-                };
+                    var fst = tokens[0];
+                    var lst = tokens[^1];
 
-                var fst = tokens[0];
-                var lst = tokens[^1];
-
-                yield return (fst * 10) + lst;
+                    yield return (fst * 10) + lst;
+                }
+                else yield return default;
             }
         }
 
         private sealed class Tokenizer(IEnumerable<TokenDefinition> definitions)
         {
-            public IEnumerable<Token> Tokenize(string toBeParsed)
-            {
-                if (string.IsNullOrWhiteSpace(toBeParsed)) return Enumerable.Empty<Token>();
-
-                return from definition in definitions ?? Enumerable.Empty<TokenDefinition>()
-                       from token in definition.FindTokens(toBeParsed)
-                       orderby token.Index ascending
-                       select token;
-            }
+            public IEnumerable<Token> Tokenize(string toBeTokenized) =>
+                from definition in definitions ?? Enumerable.Empty<TokenDefinition>()
+                from token in definition.FindTokens(toBeTokenized)
+                orderby token.Index ascending
+                select token;
         }   
 
         private sealed record Token(string Value, string Name, int Index)
@@ -58,7 +52,7 @@ namespace AdventOfCode._2023.Day_1
                 {
                     if (char.IsNumber(token.Value, 0)) return (int)(char.GetNumericValue(v, 0));
 
-                    throw new ApplicationException($"None supported token value : {v}");
+                    throw new ApplicationException($"Not supported token value : {v}");
                 }
                 else return v switch {
                     "zero" => 0,
@@ -71,25 +65,24 @@ namespace AdventOfCode._2023.Day_1
                     "seven" => 7,
                     "eight" => 8,
                     "nine" => 9,
-                    _ => throw new ApplicationException($"None supported token value : {v}"),
+                    _ => throw new ApplicationException($"Not supported token value : {v}"),
                 };
             }
         }
 
         private sealed class TokenDefinition(string pattern)
         {
-            private readonly Regex _L2Rregex = new(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            private readonly Regex _R2Lregex = new(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.RightToLeft);
+            private readonly Regex _positiveLookaheadRegex = new ($"(?=({pattern}))", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
             public IEnumerable<Token> FindTokens(string input)
             {
-                var l2rMatch = _L2Rregex.Matches(input).FirstOrDefault();
-                var r2lMatch = _R2Lregex.Matches(input).FirstOrDefault();
+                var matches = _positiveLookaheadRegex.Matches(input);
 
-                if (l2rMatch is null || r2lMatch is null) yield break;
+                var fst = matches.FirstOrDefault()?.Groups[1];
+                var lst = matches.LastOrDefault()?.Groups[1];
 
-                yield return new Token(l2rMatch.Value, l2rMatch.Name, l2rMatch.Index);
-                yield return new Token(r2lMatch.Value, r2lMatch.Name, r2lMatch.Index);
+                if (fst is not null) yield return new(fst.Value, fst.Name, fst.Index);
+                if (lst is not null) yield return new(lst.Value, lst.Name, lst.Index);
             }
 
             public static implicit operator TokenDefinition (string pattern) => new(pattern);            
