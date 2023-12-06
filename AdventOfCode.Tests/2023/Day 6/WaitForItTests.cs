da3dd450a7eab4ecdf8e2d399e21ce76123fdff9
Richard Ashman <richard.ashman@nhschoices.net>
2023-12-06 20:53:21 +0000

    Implemented second approach for Day 6 that uses a quadratic curve to solve, rather than the brute force algo I implemented first.
    Much harder to understand the code, but does run a load faster.
    
    Will try to implement both types of solution for each day going forwards, and will aim to revisit the previous five days to do something similar (if appropriate)

--- AdventOfCode.Tests/2023/Day 6/WaitForItTests.cs
+++ AdventOfCode.Tests/2023/Day 6/WaitForItTests.cs
@@ -10,30 +10,80 @@ namespace AdventOfCode.Tests._2023.Day_6
         [DataRow("Time: 44 70 70 80", "Distance: 283 1134 1134 1491", 219849)]
         [DataRow("Time: 71530",       "Distance: 940200",              71503)]
         [DataRow("Time: 44707080",    "Distance: 283113411341491",  29432455)]
-        public async Task Do(string rawTime, string rawDistance, int expectedMarginOfError)
+        public async Task Calculates_MarginOfErrorCorrectly(string rawTime, string rawDistance, int expectedMarginOfError)
         {
             var rawData = new[] { rawTime, rawDistance };
 
             var raceRecords = WaitForIt.Parse(rawData);
+
             var marginOfError = 1L;
 
             var countOfRaces = raceRecords.GetLength(0);
 
             for (var race = 0; race < countOfRaces; race++)
             {
-                var timeTakenMs = raceRecords[race, 0];
-                var distCoveredMm = raceRecords[race, 1];
+                var raceDurationMs = raceRecords[race, 0];
+                var raceDistCoveredMm = raceRecords[race, 1];
 
-                var clock = WaitForIt.TickingClock(timeTakenMs);
+                var c = WaitForIt.TickingClock(raceDurationMs);
 
-                var x = clock
-                    .Select(t => (startingSpeed: t, distance: t * (timeTakenMs - t)))
-                    .Where(r => r.distance > distCoveredMm)
+                var x = c
+                    .Select(c => (startingSpeed: c, distance: c * (raceDurationMs - c)))
+                    .Where(r => r.distance > raceDistCoveredMm)
                     .Select(r => r.startingSpeed)
                     .LongCount();
                     ;
 
                 marginOfError *= (x == 0) ? 1 : x;
+            }
+
+            Assert.AreEqual(expectedMarginOfError, marginOfError);
+        }
+
+        [DataTestMethod]
+        [DataRow("Time: 7 15 30", "Distance: 9 40 200", 288)]
+        [DataRow("Time: 44 70 70 80", "Distance: 283 1134 1134 1491", 219849)]
+        [DataRow("Time: 71530", "Distance: 940200", 71503)]
+        [DataRow("Time: 44707080", "Distance: 283113411341491", 29432455)]
+        public async Task Calculates_MarginOfErrorCorrectly_MathifiedForGiggles(string rawTime, string rawDistance, int expectedMarginOfError)
+        {
+            // d = c * (t - c) where d is distance covered, c is initial charge, and t is time taken
+            // d = -c² + ct
+            // formulae forms a quadratic curve, so we can use it to calculate those distances travelled that 
+            // exceed the previous record ...
+            // i.e. where (-c² + ct - d > 0) holds true
+
+            // quadratic equation : (-b ± √(b² - 4ac)) / 2a
+
+            var rawData = new[] { rawTime, rawDistance };
+
+            var raceRecords = WaitForIt.Parse(rawData);
+
+            var marginOfError = 1L;
+
+            var countOfRaces = raceRecords.GetLength(0);
+
+            for (var race = 0; race < countOfRaces; race++)
+            {
+                var raceDuration = raceRecords[race, 0];  // race duration in ms
+                var raceRecord = raceRecords[race, 1];    // race distance record in mm
+
+                var chargeTime1 = (-raceDuration + Math.Sqrt((raceDuration * raceDuration) - (4 * raceRecord))) / -2;
+                var chargeTime2 = (-raceDuration - Math.Sqrt((raceDuration * raceDuration) - (4 * raceRecord))) / -2;
+
+                // range = lowest charge time to highest
+
+                if (chargeTime1 > chargeTime2) (chargeTime2, chargeTime1) = (chargeTime1, chargeTime2);   
+
+                // adjust to consider we only want distances travelled that break the existing record, and we have calculated
+                // exact values on the curve
+
+                var rs = (long)Math.Ceiling((0 == chargeTime1 % 1) ? ++chargeTime1 : chargeTime1);
+                var re = (long)Math.Floor(  (0 == chargeTime2 % 1) ? --chargeTime2 : chargeTime2);
+
+                var c = (0 >= (re - rs)) ? 1 : ((re - rs) + 1);
+
+                marginOfError *= c;
             }
 
             Assert.AreEqual(expectedMarginOfError, marginOfError);
